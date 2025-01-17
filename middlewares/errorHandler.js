48df2e25bbf0e9f264b80e9bad0deefbd196cb79
𝙘𝙝𝙖𝙪𝙙𝙝𝙖𝙧𝙮 𝘼𝙄 <aichaudharybot@gmail.com>
2025-01-18 05:23:34 +0700

    Revert "Added payment handler backend with modular architecture"

--- middlewares/errorHandler.js
+++
@@ -1,7 +0,0 @@
-const errorHandler = (err, req, res, next) => {
-    console.error('Error:', err.message);
-    res.status(500).json({ error: 'Internal server error' });
-  };
-  
-  module.exports = { errorHandler };
-  
